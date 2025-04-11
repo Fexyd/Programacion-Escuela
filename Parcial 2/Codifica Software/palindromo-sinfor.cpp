6f23027e8d97381e468ef169e051f948fcead952
Fexyd <cararoja241@gmail.com>
2025-04-11 11:50:14 -0600

    main- v0.0.03

---
+++ Parcial 2/Codifica Software/palindromo-sinfor.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    char palabra[21] = "-AmolapacificapalomA";
+
+    cout << palabra[20];
+    cout << palabra[19];
+    cout << palabra[18];
+    cout << palabra[17];
+    cout << palabra[16];
+    cout << palabra[15];
+    cout << palabra[14];
+    cout << palabra[13];
+    cout << palabra[12];
+    cout << palabra[11];
+    cout << palabra[10];
+    cout << palabra[9];
+    cout << palabra[8];
+    cout << palabra[7];
+    cout << palabra[6];
+    cout << palabra[5];
+    cout << palabra[4];
+    cout << palabra[3];
+    cout << palabra[2];
+    cout << palabra[1];
+    cout << palabra[0];
+    
+    return 0;
+}
